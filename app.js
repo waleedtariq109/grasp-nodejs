0377d12c746f8a2d595326f83129b9bd55b8ef42
Waleed Tariq <waleedtariq109@outlook.com>
2024-01-11 16:57:40 +0500

    .

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const http = require("node:http");
+const http = require("http");
 
 const HOSTNAME = "127.0.0.1";
-const PORT = 3000;
+const PORT = 8000;
 
-const server = http.createServer(async (req, res) => {
-  try {
-    res.statusCode = 200;
-    res.setHeader("Set-Content", "plain/text");
-    res.end(`Hello World From: ${req.url}`);
-  } catch {}
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "plain/text");
+  res.end("Hello's everyone");
 });
 
 server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server is running on http://${HOSTNAME}:${PORT}`);
+  console.log(`The server is running on http://${HOSTNAME}:${PORT}`);
 });
