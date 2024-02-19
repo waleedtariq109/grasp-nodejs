c50c1ea0b6a19272a165a1935be2a1dd88c4dced
Waleed Tariq <waleed.tariq@manafatech.com>
2024-02-20 00:26:51 +0500

    .

--- 2-planets-project-io/index.js
+++ 2-planets-project-io/index.js
@@ -1,7 +1,14 @@
 const fs = require("fs");
 
+// Blocking/Synchronous way
 const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textInput);
 
 const textForWrite = `This is what we know about the Avacado: ${textInput}\n Created at: ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textForWrite);
+console.log("File Written!");
+
+// Non-Blocking Asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
