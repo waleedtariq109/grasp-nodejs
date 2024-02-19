5e21bf6511314403aff8d0e8873d4754b08746a7
Waleed Tariq <waleed.tariq@manafatech.com>
2024-02-20 01:51:02 +0500

    create server

--- 2-Node-Farm/index.js
+++ 2-Node-Farm/index.js
@@ -1,31 +1,33 @@
 const fs = require("fs");
+const http = require("http");
 
-// Blocking/Synchronous way
-const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
-//* console.log(textInput);
+const PORT = 8000;
+const HOSTNAME = "127.0.0.1";
 
-const textForWrite = `This is what we know about the Avacado: ${textInput}\n Created at: ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textForWrite);
-//* console.log("File Written!");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const productData = JSON.parse(data); // TODO: Use it later
 
-// Non-Blocking Asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err1, data1) => {
-  if (err1) return console.log(err1.message);
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err2, data2) => {
-    if (err2) return console.log(err2.message);
-    fs.readFile("./txt/append.txt", "utf-8", (err3, data3) => {
-      if (err3) return console.log(err3.message);
-      // Write the prepared data in `./txt/final.txt` file
-      const prepareData = `${data2}\n${data3}`;
-      fs.writeFile(
-        "./txt/final.txt",
-        `${prepareData}\n${Date.now()}`,
-        "utf-8",
-        (err4) => {
-          if (err4) return console.log(err1.message);
-          console.log("Your data has been written");
-        }
-      );
+const server = http.createServer((req, res) => {
+  // Get the request url
+  const pathName = req.url;
+  // Return response based on pathName
+  if (pathName === "/overview" || pathName === "/") {
+    res.end("This is the OVERVIEW page");
+  } else if (pathName === "/product") {
+    res.end("This is PRODUCT page");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "content-type": "application/json",
     });
-  });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is running on http://${HOSTNAME}:${PORT}`);
 });
