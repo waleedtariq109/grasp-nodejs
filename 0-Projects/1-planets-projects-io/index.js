bf46b6714c3e659b8e0857fa43126dccca85949c
Waleed Tariq <waleed.tariq@manafatech.com>
2024-02-21 19:49:40 +0500

    filter planet data

--- 0-Projects/1-planets-projects-io/index.js
+++ 0-Projects/1-planets-projects-io/index.js
@@ -3,6 +3,15 @@ const fs = require("fs");
 
 const results = [];
 
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
 fs.createReadStream("kepler_data.csv")
   .pipe(
     parse({
@@ -11,11 +20,13 @@ fs.createReadStream("kepler_data.csv")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(results);
+    console.log(`${results.length} habitable planets found!`);
   });
