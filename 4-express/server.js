1000786363fd4d67c040736c8d1fa246294bd42a
Waleed Tariq <waleed.tariq@manafatech.com>
2024-02-23 21:30:42 +0500

    .

--- 4-express/server.js
+++ 4-express/server.js
@@ -4,8 +4,35 @@ const PORT = 3000;
 
 const app = express();
 
+const friends = [
+  {
+    id: 0,
+    name: "Ahmed Shabir",
+  },
+  {
+    id: 1,
+    name: "Umair Asif",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello Worls");
+  res.send("Hello");
+});
+
+app.get("/friends", (req, res) => {
+  res.status(200).json(friends);
+});
+
+app.get("/friends/:id", (req, res) => {
+  const friendsId = req.params?.id;
+  const friend = friends[friendsId];
+
+  if (!friend)
+    res.status(404).json({
+      error: "Friend not found",
+    });
+
+  res.status(200).json(friend);
 });
 
 app.listen(PORT, () => {
