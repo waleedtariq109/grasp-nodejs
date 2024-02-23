3f161b7b1169bfb4ca391f8ce45b0b3f7b8a3405
Waleed Tariq <waleed.tariq@manafatech.com>
2024-02-23 16:26:22 +0500

    .

--- 3-embeded-js-template/server.js
+++ 3-embeded-js-template/server.js
@@ -5,18 +5,52 @@ const ejs = require("ejs");
 const PORT = 3000;
 const HOSTNAME = "127.0.0.1";
 
-const template = fs.readFileSync("./src/views/index.ejs", "utf-8");
+// Getting Data
 
-const data = {
-  message: "Hello, EJS without Express!",
-};
+const navbarDataJSON = fs.readFileSync("./database/app.json", "utf-8");
+const appDataJSON = fs.readFileSync("./database/data.json", "utf-8");
 
-const compiledTemplate = ejs.compile(template);
+//Parsed JSON
+const navbarData = JSON.parse(navbarDataJSON);
+const appData = JSON.parse(appDataJSON);
+
+// Reading Template
+const navbarTemplate = fs.readFileSync("./src/components/navbar.ejs", "utf-8");
+const mainTemplate = fs.readFileSync("./src/layouts/main.ejs", "utf-8");
+const homepageTemplate = fs.readFileSync("./src/views/home.ejs", "utf-8");
+const historyTemplate = fs.readFileSync("./src/views/history.ejs", "utf-8");
+const operationsTemplate = fs.readFileSync(
+  "./src/views/operations.ejs",
+  "utf-8"
+);
+
+// Compile Template
+const compiledNavbar = ejs.compile(navbarTemplate);
+const compiledMain = ejs.compile(mainTemplate);
+const compledHomePage = ejs.compile(homepageTemplate);
+const compiledHistoryPage = ejs.compile(historyTemplate);
+const compiledOperationPage = ejs.compile(operationsTemplate);
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end(compiledTemplate(data));
+  const renderNavbar = compiledNavbar({
+    siteName: navbarData?.siteName,
+    navLinks: navbarData?.navbarLinks,
+  });
+  const renderHomePage = compledHomePage({ home: appData?.home });
+  const renderHistoryPage = compiledHistoryPage({ history: appData?.history });
+  const renderOperationsPage = compiledOperationPage({
+    operations: appData?.operations,
+  });
+  const renderedMain = compiledMain({
+    navbar: renderNavbar,
+    path: req.url,
+    home: renderHomePage,
+    operations: renderOperationsPage,
+    history: renderHistoryPage,
+  });
+  res.end(renderedMain);
 });
 
 server.listen(PORT, HOSTNAME, () => {
