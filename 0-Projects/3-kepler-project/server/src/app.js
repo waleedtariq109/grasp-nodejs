db1b6f4f4b3afff805369ce10ee84cf20f4ad319
Waleed Tariq <waleed.tariq@manafatech.com>
2024-02-26 18:47:04 +0500

    .

--- 0-Projects/3-kepler-project/server/src/app.js
+++ 0-Projects/3-kepler-project/server/src/app.js
@@ -1,16 +1,29 @@
+const path = require("path");
 const express = require("express");
+
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
   })
 );
+
+app.use(morgan("combined"));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetRouter);
+app.use(launchesRouter);
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
---
+++ 0-Projects/3-kepler-project/server/src/models/launches.model.js
@@ -0,0 +1,16 @@
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Exp X",
+  rocket: "Explore IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "kepler-422 b",
+  customers: ["Nasa", "SpaceX"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = { launches };
